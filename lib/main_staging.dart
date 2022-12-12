e16b3cd7035a178113d4fb7a41d949aa7fdc019b
o.zaim <o.zaim@newlinetechnologies.net>
2022-12-12 10:12:46 +0200

    Initial commit

---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:demo_app/app/app.dart';
+import 'package:demo_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
---
+++ test/app/view/app_test.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:demo_app/app/app.dart';
+import 'package:demo_app/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
